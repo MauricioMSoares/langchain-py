7644ead7ef922033e7e6b9a19ee40e1b5b958746
Mauricio Soares <mauricio.massayuki@gmail.com>
2026-01-16 09:51:39 -0300

    added chatbot example script

--- chat_models.py
+++ chat_models.py
@@ -1,3 +1,5 @@
+# Chat model example
+
 from langchain_openai.chat_models import ChatOpenAI
 from langchain.messages import AIMessage, HumanMessage, SystemMessage
 
@@ -8,4 +10,22 @@ messages = [
     SystemMessage(content="You are a helpful assistant that translates English to Spanish."),
     HumanMessage(content="I love programming.")
 ]
+
 chat(messages)
+
+
+# Conversation example
+
+chat2 = ChatOpenAI(temperature=0, max_tokens=100)
+
+context = [SystemMessage(content="You are a friendly chatbot that likes to have conversations.")]
+
+while True:
+    user_message = input("You: ")
+    context.append(HumanMessage(content=user_message))
+    if user_message.lower() == "exit":
+        break
+    response = chat2(context)
+
+    context.append(response)
+    print("AI: ", response.content)
